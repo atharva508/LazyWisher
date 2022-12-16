5f3138b7e1b6a353776cd4bfc2ea62ff0d2047f6
atharva508 <85646827+atharva508@users.noreply.github.com>
2022-12-16 10:13:40 +0530

    Update what_message.py
    Increased the waiting time for the message

--- what_message.py
+++ what_message.py
@@ -4,4 +4,4 @@ from datetime import datetime
 def send_wish(grp_id,message):
     now_hour = datetime.now().hour
     now_minute = datetime.now().minute
-    pywhatkit.sendwhatmsg_to_group(grp_id,message,now_hour,now_minute+1,15,True,5)
+    pywhatkit.sendwhatmsg_to_group(grp_id,message,now_hour,now_minute+2,15,True,5)
