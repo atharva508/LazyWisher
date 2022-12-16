a9d5ffb4218c013e3b69a6a09a142d56f51b890a
atharva508 <85646827+atharva508@users.noreply.github.com>
2022-12-16 10:00:15 +0530

    Delete what_testing.py

--- what_testing.py
+++
@@ -1,6 +0,0 @@
-import pywhatkit
-from datetime import datetime
-def send_wish(grp_id,message):
-    now_hour = datetime.now().hour
-    now_minute = datetime.now().minute
-    pywhatkit.sendwhatmsg_to_group("LfE5bH5JaFdF9roT4TZlzE",message,now_hour,now_minute+1)
