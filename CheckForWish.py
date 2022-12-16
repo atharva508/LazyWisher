9525c5f080ba88552e364953f9b9d635946c8232
atharva508 <85646827+atharva508@users.noreply.github.com>
2022-12-16 10:05:38 +0530

    Update CheckForWish.py

--- CheckForWish.py
+++ CheckForWish.py
@@ -1,3 +1,4 @@
+#this module checks the file to see if any events coicide with the current day
 import pandas as pd
 from datetime import date
 
@@ -6,7 +7,6 @@ def checkDatabase():
     today_month =date.today().month
     family_data = pd.read_csv('family_Data.csv')
     family_data = family_data[(family_data['Month'] == today_month) & (family_data['Day'] == today_day)]
-    print(family_data)
     return family_data
     
 if __name__ == "__main__":
